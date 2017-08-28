7f1a59c632abab00c7ae7ea2eaca463da01cc915
Mihai Postelnicu <mpostelnicu@gmail.com>
2017-08-28 15:44:11 +0300

    copyright update

--- validator/src/main/java/org/devgateway/jocds/ApplicationLoader.java
+++ validator/src/main/java/org/devgateway/jocds/ApplicationLoader.java
@@ -1,3 +1,8 @@
+/*
+ * Copyright (c) 2017. Development Gateway and contributors. All rights reserved.
+ * Licensed under the MIT license. See LICENSE file in the project root for details.
+ */
+
 package org.devgateway.jocds;
 
 import org.slf4j.Logger;

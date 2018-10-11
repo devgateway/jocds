4fda28484e8763b226a581837386f847cddc7807
Mihai Postelnicu <mpostelnicu@gmail.com>
2018-10-11 16:21:04 +0300

    #34 moved test resource to own package

--- jocds-cli/src/test/java/org/devgateway/jocds/test/TestOcdsValidatorRelease.java
+++ jocds-cli/src/test/java/org/devgateway/jocds/test/TestOcdsValidatorRelease.java
@@ -1,9 +1,9 @@
 /*
- * Copyright (c) 2017. Development Gateway and contributors. All rights reserved.
+ * Copyright (c) 2018. Development Gateway and contributors. All rights reserved.
  * Licensed under the MIT license. See LICENSE file in the project root for details.
  */
 
-package test;
+package org.devgateway.jocds.test;
 
 import com.github.fge.jsonschema.core.report.ProcessingReport;
 import org.devgateway.jocds.OcdsValidatorConstants;

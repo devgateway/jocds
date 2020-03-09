2924262562eae5c5404dcc0ef674474f1ded9c1d
mpostelnicu <mpostelnicu@gmail.com>
2020-03-09 11:15:48 +0200

    added conditional on bean for validator, so we can configure validator externally

--- jocds-core/src/main/java/org/devgateway/jocds/ValidatorConfiguration.java
+++ jocds-core/src/main/java/org/devgateway/jocds/ValidatorConfiguration.java
@@ -7,6 +7,7 @@ package org.devgateway.jocds;
 
 import com.fasterxml.jackson.annotation.JsonInclude.Include;
 import com.fasterxml.jackson.databind.SerializationFeature;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
@@ -18,6 +19,7 @@ import java.util.TimeZone;
  * Created by mpostelnicu on 7/7/17.
  */
 @Configuration
+@ConditionalOnMissingBean(Jackson2ObjectMapperBuilder.class)
 public class ValidatorConfiguration {
 
     @Bean
